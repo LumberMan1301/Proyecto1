9db174fd21ea64ffccd798e53450da747363f0ba
Mariano <marianomm@rocketmail.com>
2018-03-20 15:42:26 -0600

    Comienza La creacion de las clases de tipo Lista, para la ubicacion de los enemigos

--- src/Listas/Lista.java
+++ src/Listas/Lista.java
@@ -1,4 +1,28 @@
 package Listas;
 
-public class Lista {
+import Nodo.Nodo;
+
+/**
+ * clase Principal de Listas
+ */
+
+public abstract class Lista<T> {
+    /**
+     * atributos de la clase lista, uno de Tipo Nodo para almacenar informacion, y otro de tipo Int para hacer
+     * un "indice"
+     */
+    private Nodo primero;
+    private int capacidad;
+
+    /**
+     * constructor de la clase lista
+     */
+    public Lista() {
+        this.capacidad=0;
+        this.primero = null;
+    }
+
+    
+
+
 }
