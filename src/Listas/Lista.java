d5fcc01e9f3428f21add9aea5f08e591bcc44be1
Mariano <marianomm@rocketmail.com>
2018-03-20 21:46:53 -0600

    Progreso con la construccion de las listas

--- src/Listas/Lista.java
+++ src/Listas/Lista.java
@@ -11,7 +11,7 @@ public abstract class Lista<T> {
      * atributos de la clase lista, uno de Tipo Nodo para almacenar informacion, y otro de tipo Int para hacer
      * un "indice"
      */
-    private Nodo primero;
+    private Nodo referencia;
     private int capacidad;
 
     /**
@@ -19,10 +19,40 @@ public abstract class Lista<T> {
      */
     public Lista() {
         this.capacidad=0;
-        this.primero = null;
+        this.referencia = null;
     }
 
-    
+    /**
+     * metodo para revisar si la lista esta vacia
+     */
+    public boolean estaVacia(){
+        if (this.capacidad==0)
+            return true;
+        else
+            return false;
+    }
 
+    /**
+     * metodo para comprobar si un nodo Existe
+     */
+    public boolean existe(Nodo nodo){
+        if (estaVacia()){
+            return false;
+        }else{
+            Nodo aux = this.referencia;
+            while(aux != null){
+                if(aux.getDato().equals(nodo.getDato()))
+                    return true;
+                else
+                    aux=aux.getSiguiente();
+            }return false;
+        }
+    }
 
+    /**
+     * metodo para agregar al final de la lista
+     */
+    public void agregar(Nodo nodo){
+
+    }
 }
