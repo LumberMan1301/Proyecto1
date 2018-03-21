bbb6e892fe1b53ba6e84e4c6f779d79917fd514a
Mariano <marianomm@rocketmail.com>
2018-03-21 00:38:50 -0600

    Progreso con la construccion de las listas

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import EstructuraDeDatosLineal.Listas.Lista;
+import EstructuraDeDatosLineal.Listas.ListaDoble;
 
 public class Main {
     public static void main (String [] args){
@@ -6,6 +7,11 @@ public class Main {
         l1.agregar("mariano");
         l1.agregar("karla");
         l1.imprimirL();
+
+        ListaDoble ld1 = new ListaDoble();
+        ld1.agregar("maria");
+        ld1.agregar("steven");
+        ld1.imprimirL();
 
 
 
